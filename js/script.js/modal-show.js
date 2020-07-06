a1c7647cf3e092c17c6bc7fa4f0ee71c282b9c37
Alexanderhtml9 <lion99976@mail.ru>
2020-07-06 06:44:59 +0300

    добавление файла js и изменения

---
+++ js/script.js/modal-show.js
@@ -0,0 +1,33 @@
+var ouradress = document.querySelector(".our-adress");
+var loginPopup = document.querySelector(".modal-login");
+var loginClose = loginPopup.querySelector(".modal-close");
+var loginForm = loginPopup.querySelector(".form-acting");
+var yourname = loginPopup.querySelector(".your-name");
+var youremail = loginPopup.querySelector(".your-email");
+
+loginLink.addEventListener("click", function () {
+  evt.preventDefault();
+  loginPopup.classList.add("modal-show");
+});
+
+loginClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginPopup.classList.remove("modal-show");
+  loginLogin.focus();
+});
+
+loginForm.addEventListener("submit", function (evt) {
+  if (!yourname.value || !youremail.value)  {
+  evt.preventDefault();
+  }
+});
+
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (loginPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      loginPopup.classList.remove("modal-show");
+    }
+  }
+});
